03f291d945f7303705a6f8b5b45355d8f23dfd0a
Pradhyumn Yadav <73153956+Pradhyumn1@users.noreply.github.com>
2024-01-24 10:11:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# app.py
+
+from flask import Flask, render_template, request
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        input_sms = request.form['email']
+        # 1. preprocess
+        transformed_sms = transform_text(input_sms)
+        # 2. vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+            return render_template('show.html', prediction=1)
+        else:
+            return render_template('show.html', prediction=0)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
